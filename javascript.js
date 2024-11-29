7842265a077c7dd6f0d3eec17e7be6788e47a264
MirkoDD <mirkodidio111@gmail.com>
2024-11-29 14:20:30 +0100

    added:first_concept_of_program

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
+/* 
+1 - creo un ciclo definito per dirgli di contare da 1 a 100 
+2 - al suo interno utilizzo più condizionali per includere/escludere la possibilità che un numero sia multiplo di 5 e 3 (si? riporto la scritta "FizzBuzz").
+  - SE non lo è eseguo una seconda condizionale verificando se sia multiplo di 5 (si? riporto la scritta "Buzz").
+  - SE non lo è eseguo una terza condizionale verificando se sia un multiplo di 3 (si? riporto la scritta "Fizz").
+  - SE non non è nessuno dei precendenti riporto il numero sul grezzo nella console.
+  - SE non è nessuno dei precedenti rileggite cosa hai scritto.
+*/
